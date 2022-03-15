4555e93bc53bee1638da941ded5b4df9a957c8da
Machmachi Mohamed <61361554+mimothaking@users.noreply.github.com>
2022-03-15 16:11:04 +0100

    Login + Registration Database

---
+++ LabNorVida/LabNorVida/DB.cs
@@ -0,0 +1,44 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNorVida
+{
+
+    
+
+    class DB
+    {
+
+        MySqlConnection connection = new MySqlConnection("server=localhost;port=3306;username=root;password=;database=norvida");
+
+
+        //Function to open connection
+        public void openConnection()
+        {
+            if(connection.State == System.Data.ConnectionState.Closed)
+            {
+                connection.Open();
+            }
+        }
+
+        //Function to close connection
+        public void closeConnection()
+        {
+            if (connection.State == System.Data.ConnectionState.Open)
+            {
+                connection.Close();
+            }
+        }
+
+
+        //Function to return the connection
+        public MySqlConnection GetConnection()
+        {
+            return connection;
+        }
+    }
+}
--- LabNorVida/LabNorVida/MainWindow.xaml.cs
+++ LabNorVida/LabNorVida/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using MySql.Data.MySqlClient;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data;
 
 namespace LabNorVida
 {
@@ -20,9 +22,156 @@ namespace LabNorVida
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        
+
+
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            DB db = new DB();
+
+            String username = Username.Text;
+            String password = Password.Text;
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `technicien` WHERE `username` = @Username AND `password` = @Password", db.GetConnection());
+
+            command.Parameters.Add("@Username", MySqlDbType.VarChar).Value = username;
+            command.Parameters.Add("@Password", MySqlDbType.VarChar).Value = password;
+
+            adapter.SelectCommand = command;
+
+            adapter.Fill(table);
+
+
+            //Checking if the user exists or not !
+            if (table.Rows.Count > 0)
+            {
+                this.Hide();
+                vrac vracpage = new vrac();
+                vracpage.Show();
+            }
+            else
+            {
+                MessageBox.Show("NO");
+            }
+
+        }
+
+        private void Password_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+           
+        }
+
+        private void Username_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Username_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            String uname = Username.Text;
+            if (uname.Equals("Username"))
+            {
+                Username.Text = "";
+            }
+        }
+
+        private void Password_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            String pswd = Password.Text;
+            if (pswd.Equals("Password"))
+            {
+                Password.Text = "";
+            }
+        }
+
+
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void RegisterButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            //Adding a new user
+
+            DB db = new DB();
+            MySqlCommand command = new MySqlCommand("INSERT INTO `technicien`(`Fullname`, `Username`, `Password`) VALUES (@Fullname, @Username, @Password)", db.GetConnection());
+
+            command.Parameters.Add("@Fullname", MySqlDbType.VarChar).Value = Fullname.Text;
+            command.Parameters.Add("@Username", MySqlDbType.VarChar).Value = UsernameR.Text;
+            command.Parameters.Add("@Password", MySqlDbType.VarChar).Value = PasswordR.Text;
+
+            //open Connection
+            db.openConnection();
+
+            if(checkUsername())
+            {
+                MessageBox.Show("Username already Used!!");
+            }
+            else
+            { 
+            //execute query
+            if (command.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("Account Created!");
+            }
+            else
+            {
+                MessageBox.Show("Error!");
+            }
+            }
+            //close conn
+            db.closeConnection();
+
+            
+        }
+
+        //Checking if username already exists
+        public Boolean checkUsername()
+        {
+            DB db = new DB();
+
+            String username = Username.Text;
+            
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `technicien` WHERE `username` = @Username", db.GetConnection());
+
+            command.Parameters.Add("@Username", MySqlDbType.VarChar).Value = username;
+            
+
+            adapter.SelectCommand = command;
+
+            adapter.Fill(table);
+
+
+            //Checking if this user already exists or not !
+            if (table.Rows.Count > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+           
         }
     }
 }
