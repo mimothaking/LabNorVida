58797e4a012d9b28fbd4815bc7310cc6cd98c084
Machmachi Mohamed <61361554+mimothaking@users.noreply.github.com>
2022-03-30 01:26:48 +0000

    calculations v1

--- LabNorVida/LabNorVida/Globals.cs
+++ LabNorVida/LabNorVida/Globals.cs
@@ -8,6 +8,46 @@ namespace LabNorVida
 {
     class Globals
     {
+        //Techniciens variables
         public static string usrName;
+
+        //Echantillon variables
+        public static int referance;
+        public static DateTime dateEssai;
+        public static float classGra;
+        public static int typeConteneur;
+        public static float mVrac;
+        public static float mporosite;
+        public static string cBalance;
+        public static string cConteneur;
+        public static string cThermo;
+        public static string cEtuve;
+
+        //Vrac variables
+        public static float mcv1;
+        public static float mcv2;
+        public static float mcv3;
+        public static float mce1;
+        public static float mce2;
+        public static float mce3;
+        public static float me1;
+        public static float me2;
+        public static float me3;
+        public static float vc1;
+        public static float vc2;
+        public static float vc3;
+        public static float mvv1;
+        public static float mvv2;
+        public static float mvv3;
+
+        //Poro variables
+        public static float mvr1;
+        public static float mvr2;
+        public static float mvr3;
+        public static float pi1;
+        public static float pi2;
+        public static float pi3;
+
+
     }
 }
--- LabNorVida/LabNorVida/vrac.xaml.cs
+++ LabNorVida/LabNorVida/vrac.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
+using System.Data;
 
 namespace LabNorVida
 {
@@ -27,6 +29,52 @@ namespace LabNorVida
         private void window_Loaded(object sender, RoutedEventArgs e)
         {
             label.Content = Globals.usrName;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Adding a new echantillon
+            DB db = new DB();
+            DataTable table = new DataTable();
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("INSERT INTO `echantillon`(`referance `, `dateEssai`, `classeGra`,`typeConteneur `,`vrac `,`porosite `,`cBalance `,`cConteneur `,`cThermo `,`cEtuve ` ) VALUES (@ref, @date, @classe, @type, @cbalance, @cconteneur, @cthermo, @cetuve)", db.GetConnection());
+
+            command.Parameters.Add("@ref", MySqlDbType.Int32).Value = referance.Text ;
+            command.Parameters.Add("@date", MySqlDbType.DateTime).Value = dateEchantillon.Text;
+            command.Parameters.Add("@classe", MySqlDbType.Float).Value = classGra.Text;
+            command.Parameters.Add("@type", MySqlDbType.Int32).Value = typeConteneur.Text;
+            command.Parameters.Add("@cbalance", MySqlDbType.VarChar).Value = balance.Text;
+            command.Parameters.Add("@cconteneur", MySqlDbType.VarChar).Value = conteneur.Text;
+            command.Parameters.Add("@cthermo", MySqlDbType.VarChar).Value = thermo.Text;
+            command.Parameters.Add("@cetuve", MySqlDbType.VarChar).Value = etuve.Text;
+
+            adapter.SelectCommand = command;
+
+            adapter.Fill(table);
+
+
+            //open Connection
+            db.openConnection();
+
+            
+                //execute query
+                if (command.ExecuteNonQuery() == 1)
+                {
+                      Validation valid = new Validation();
+                       this.Hide();
+                      valid.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Error!");
+                }
+            
+
+            //close conn
+            db.closeConnection();
+
+
         }
     }
 }
